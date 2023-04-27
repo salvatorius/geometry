9cb89cfffdfa06115e8be01581578ef6a0b9101e
Salvatorius <99340471+salvatorius@users.noreply.github.com>
2023-04-27 02:51:11 -0300

    Adding parameterized tests from lists of TestCases coming from either a constant or a fixture

--- tests/test_4_parametrized_with_fixtures.py
+++ tests/test_4_parametrized_with_fixtures.py
@@ -15,12 +15,12 @@ def star():
     return [(6, 3),(7, 5),(9, 5),(7.5, 6.5),(8, 9),(6, 7.5),(4, 9),(4.5, 6.5),(3, 5),(5, 5)]
 
 
-@mark.parametrize("coordinates,expected_area", [
+@mark.parametrize("coord_fixture_name,expected_area", [
     ('trapeze', 2),
     ('bird', 3),
     ('star', 14.5)
     ])
-def test_calc_polygon_area_given_its_points_from_fixture(coordinates, expected_area, request):
-    points = request.getfixturevalue(coordinates)
+def test_calc_polygon_area_given_its_points_from_fixture(coord_fixture_name, expected_area, request):
+    points = request.getfixturevalue(coord_fixture_name)
     polygon = Polygon(points)
     assert polygon.calc_area() == expected_area
---
+++ tests/test_5_parametrized_from_unified_constant.py
@@ -0,0 +1,45 @@
+from typing import List, Tuple
+from dataclasses import dataclass
+from pytest import approx, mark
+from geometry.areas.polygon import Polygon
+
+@dataclass
+class ParamTestCase:
+    points: List[Tuple]
+    area: float = 1
+
+TestCases = List[ParamTestCase]
+
+POLYGONS : TestCases = [
+    # Isosceles Triangle
+    ParamTestCase(points=[(0, 3), (4, -1), (-4, -1)], area = 16),
+    # Equilateral Triangle
+    ParamTestCase(points=[(-3, 0), (1.31, 4.08), (2.69, -1.69)], area = 15.2495),
+    # Square 1
+    ParamTestCase(points=[(1, 4), (3, 0), (-1, -2), (-3, -2)], area = 16),
+    # Square 2
+    ParamTestCase(points=[(0,2), (2,1), (1,-1), (0,-1)], area = 4),
+    # Diamond
+    ParamTestCase(points=[(0.5,2.5),(2.5,-0.5),(0.5,-3.5),(-1.5,-0.5)], area = 12),
+    # Trapeze
+    ParamTestCase(points=[(0, 2), (1, 0), (-1, -3), (-3, 1)], area = 10.5),
+    # Parallelogram
+    ParamTestCase(points=[(0,4),(2,0),(1,-3),(-1,1)], area = 10),
+    # Pentagon
+    ParamTestCase(points=[(-1,-1),(1.85,-0.07),(1.85,2.93),(-1.01,3.85),(-2.77,1.42)], area = 15.486),
+    # Heptagon
+    ParamTestCase(points=[(1,-2),(-1.1,-2.3),(-2.64,-0.84),(-2.46,1.27),(-0.7,2.45),(1.32,1.81),(2.08,-0.17)], area = 16.3427),
+    # Bird
+    ParamTestCase(points=[(0, 3), (1, 3), (2, 2), (3, 2), (2, 1), (1, 1)], area = 3),
+    # Star
+    ParamTestCase(points=[(6, 3),(7, 5),(9, 5),(7.5, 6.5),(8, 9),(6, 7.5),(4, 9),(4.5, 6.5),(3, 5),(5, 5)], area = 14.5),
+    # Crown
+    ParamTestCase(points=[(-4,-2),(-6,3),(-4,1),(-3,3),(-2,1),(-1,3),(0,1),(1,3),(2,1),(3,3),(4,1),(6,3),(4,-2)], area=38)
+    # ...
+    ]
+
+
+@mark.parametrize("test_case", POLYGONS)
+def test_calc_polygon_area_given_its_points_from_constant_list_item(test_case: ParamTestCase):
+    polygon = Polygon(test_case.points)
+    assert polygon.calc_area() == approx(test_case.area, 1e-04)
